322ef27577ae4f2d62fb65f1c777814396e1b662
AyeshaAreej <ayeshaareej222@gmail.com>
2022-09-30 10:57:19 +0500

    TypeScript Basics

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var message = "Welcome Back";
+console.log(message);
+// Variable Types
+var isBeginner = true;
+var total;
+total = 1;
+var name = 'Ayesha';
+// Template Literal-Multiple lines with embedded expressions
+var sentence = "My name is ".concat(name, "\nI am learning TypeScript and it is pretty good");
+console.log(sentence);
---
+++ main.ts
@@ -0,0 +1,15 @@
+export {}
+let message="Welcome Back";
+console.log(message);
+
+// Variable Types
+
+let isBeginner : boolean=true;
+let total:number;  
+total=1;
+let name:string='Ayesha';
+
+// Template Literal-Multiple lines with embedded expressions
+let sentence:string=`My name is ${name}
+I am learning TypeScript and it is pretty good`
+console.log(sentence)
